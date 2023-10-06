c1e0d86e3cff5a03816a4de2b7ce22a04722a14c
Aayushi Mittal <aayushimittal@Aayushis-MacBook-Pro.local>
2023-10-07 01:05:16 +0530

    Pictionary app

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,131 @@
+package com.example.android.guesstheword.screens.game
+
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+
+class GameViewModel : ViewModel(){
+
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
+
+    // The current score
+   private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    private val _isGameFinished = MutableLiveData<Boolean>()
+    val isGameFinished : LiveData<Boolean>
+        get() = _isGameFinished
+
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 10000L
+    }
+
+    private val timer : CountDownTimer
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time);
+    }
+
+    init {
+        Log.i("GameViewModel", "created!")
+        resetList();
+        nextWord();
+        _score.value =0
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+            override fun onTick(millisUntilFinished : Long) {
+               _currentTime.value = (millisUntilFinished/ ONE_SECOND);
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _isGameFinished.value = true
+            }
+        }
+        timer.start();
+    }
+    override fun onCleared() {
+        super.onCleared();
+        Log.i("GameViewModel", "destroyed!")
+        timer.cancel();
+
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            resetList();
+        }
+        _word.value = wordList.removeAt(0)
+    }
+
+   fun onSkip() {
+        _score.value = score.value?.minus(1);
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = score.value?.plus(1);
+        nextWord()
+    }
+
+    fun onGameFinishComplete (){
+        _isGameFinished.value = false
+    }
+
+
+}
--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -31,6 +33,7 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
+    private lateinit var viewModel: ScoreViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -45,16 +48,29 @@ class ScoreFragment : Fragment() {
                 container,
                 false
         )
-
         // Get args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
-        binding.scoreText.text = scoreFragmentArgs.score.toString()
-        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+        viewModel = ViewModelProvider(this, ScoreViewModelFactory(scoreFragmentArgs.score))
+            .get(ScoreViewModel:: class.java);
 
+//        binding.playAgainButton.setOnClickListener {
+//            viewModel.playAgain()
+//        }
+
+        binding.scoreViewModel = viewModel
+        binding.setLifecycleOwner(this);
+
+//        viewModel.score.observe(this, Observer {
+//            binding.scoreText.text = scoreFragmentArgs.score.toString()
+//        })
+
+        viewModel.eventPlayAgain.observe(this, Observer {playAgain ->
+            if(playAgain == true){
+                findNavController().navigate(ScoreFragmentDirections.actionRestart());
+                viewModel.playAgainComplete()
+            }
+        })
         return binding.root
     }
 
-    private fun onPlayAgain() {
-        findNavController().navigate(ScoreFragmentDirections.actionRestart())
-    }
 }
